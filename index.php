c124691bc2a17347b1357d0971ac751c44ce898b
Natalia Hańczka <UL0269767@edu.uni.lodz.pl>
2023-10-24 23:43:58 +0200

    Change the language to English

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
-<html lang="pl-PL"></html>
+<html lang="en"></html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style/style.css">
-    <title>ONP Kalkulator</title>
+    <title>RPN Calculator</title>
 </head>
 <body>
     <div>
-        <h1 class="header">Podaj wyrażenie do policzenia</h1>
+        <h1 class="header">RPN calculator</h1>
     </div>
     <?php
     if(file_exists("views/form.php"))include_once "views/form.php";
--- views/form.php
+++ views/form.php
@@ -1,8 +1,8 @@
 <div class = "form">
     <form method="post" action = "">
-        <label for="conventionalNotation">Zapis konwencjonalny: </label>
+        <label for="conventionalNotation">Conventional notation: </label>
         <input type="text" name="conventionalNotation" id="conventionalNotation" placeholder="-2 + 4 * (2 + 3) -1" required>
-        <input type="submit" value="Policz">
+        <input type="submit" value="Count">
     </form>
     <?php
     include_once("lib/form_bg.php");
