3b984f431a924f10d1e18e34b4c073e193ce6321
Natalia Hańczka <UL0269767@edu.uni.lodz.pl>
2023-10-25 03:09:00 +0200

    update comment in RPNIO

--- interfaces/RPNIO.php
+++ interfaces/RPNIO.php
@@ -7,6 +7,9 @@ interface RPNIO
     Widziałam tu dwa rozwiązania,natomiast żadna nie była tą zaproponowaną.
     Wersja pierwsza: funkcja statyczna
     Wersja druga: bez przekazanego parametru do funkcji, tylko pobranie jej z pola instancji, na której wywołujemy funkcję.
+
+    Oczywiście zaproponowana wersja jest wykonalna, natomiast nie widziałam sensu w tworzeniu instancji obiektu, wywołania funkcji 
+    i przekazania wartości pola ręcznie
      */
     public static function calculate(string $input):float;
 }
